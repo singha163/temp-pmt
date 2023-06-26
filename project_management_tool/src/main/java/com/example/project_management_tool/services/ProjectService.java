508749ef92aeb4db5d41b293e144754d7a78e796
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-26 13:05:54 -0700

    Midterm - Add, update and delete working fully
    Security and auth files need to be checked

--- project_management_tool/src/main/java/com/example/project_management_tool/services/ProjectService.java
+++ project_management_tool/src/main/java/com/example/project_management_tool/services/ProjectService.java
@@ -1,11 +1,9 @@
 package com.example.project_management_tool.services;
 
-import com.example.project_management_tool.domain.Backlog;
 import com.example.project_management_tool.domain.Project;
 import com.example.project_management_tool.domain.User;
 import com.example.project_management_tool.exceptions.ProjectIdException;
 import com.example.project_management_tool.exceptions.ProjectNotFoundException;
-import com.example.project_management_tool.repositories.BacklogRepository;
 import com.example.project_management_tool.repositories.ProjectRepository;
 import com.example.project_management_tool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +17,7 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    @Autowired
-    private BacklogRepository backlogRepository;
+
 
     @Autowired
     private UserRepository userRepository;
@@ -46,16 +43,6 @@ public class ProjectService {
             project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
-            if(project.getId()==null){
-                Backlog backlog = new Backlog();
-                project.setBacklog(backlog);
-                backlog.setProject(project);
-                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-            }
-
-            if(project.getId()!=null){
-                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
-            }
 
             return projectRepository.save(project);
         }catch (Exception e){
--- project_management_tool/src/main/java/com/example/project_management_tool/web/ProjectController.java
+++ project_management_tool/src/main/java/com/example/project_management_tool/web/ProjectController.java
@@ -8,14 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 //Controller layer to pass all CRUD operations to service layer
